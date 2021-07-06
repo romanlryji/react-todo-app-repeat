150f6cced4361da494b46d1382654f2b19b4e175
romanlryji <romanlryji@yahoo.co.uk>
2021-07-06 22:32:33 +0300

    spread

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 
-const element = <h1>Hello from Create React App</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
 
 // npx create-react-app react-todo-app
