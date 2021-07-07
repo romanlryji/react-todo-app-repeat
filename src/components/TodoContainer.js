f7716504b68e00aa30113756ecb2feeab90e3cb0
romanlryji <romanlryji@yahoo.co.uk>
2021-07-07 20:19:57 +0300

    newTodo

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -51,6 +51,14 @@ class TodoContainer extends React.Component {
   }
 
   addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
     console.log(title)
   }
 
